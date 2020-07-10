a1164c3bb1fee883a409657d7bb75092b9c2079e
sawich <sawich@angeldev.org>
2020-07-10 07:26:11 +0300

    games, deps

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import { resolve } from "path";
+import { join } from "path";
 
 const app = express();
 
 app.get("/packages/:game", function (req, res) {
-  res.sendFile(`./public/packages/${req.params.game}.json`, { root: resolve(__dirname, "../") });
+  res.sendFile(`./public/packages/${req.params.game}.json`, { root: join(__dirname, "../") });
 });
 
 app.get("/scripts/:uuid", function (req, res) {
-  res.sendFile(`./public/scripts/${req.params.uuid}.json`, { root: resolve(__dirname, "../") });
+  res.sendFile(`./public/scripts/${req.params.uuid}.json`, { root: join(__dirname, "../") });
+});
+
+app.get("/games/:game", function (req, res) {
+  res.sendFile(`./public/games/${req.params.game}.json`, { root: join(__dirname, "../") });
 });
 
 app.listen(3000, function () {
